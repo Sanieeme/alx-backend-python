c36c772360a133eab564b0c2f50eb665c19bf372
Sanieeme <shoneesani@gmail.com>
2024-07-04 10:13:38 +0000

    basic notation annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""" funtion"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns their sum as a float
+    Args:
+        a(float): argument
+        b(float): argument
+    Return: a + b
+    """
+    return float(a + b)
