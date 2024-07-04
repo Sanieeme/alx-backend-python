803ee93b4249aca350aed2c0938b653955286e47
Sanieeme <shoneesani@gmail.com>
2024-07-04 10:15:43 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+
+    Args:
+        str1(str): string 1
+        str2(str): string 2
+    Return: str1+str2
+    """
+    return str1 + str2
