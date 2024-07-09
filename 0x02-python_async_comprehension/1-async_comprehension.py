da9df25791532b397a7fb596b4a32866e40035e9
Sanieeme <shoneesani@gmail.com>
2024-07-09 09:56:08 +0000

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""import modules"""
+import importlib.util
+import asyncio
+module_name = '0-async_generator'
+file_path = '0-async_generator.py'
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+async_generator = module.async_generator
+
+
+async def async_comprehension():
+    """
+    async comprehension
+    Args:
+        None
+    Return:
+    """
+    result = [i async for i in async_generator()]
+    return result
