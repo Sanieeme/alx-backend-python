998a189f816b65c9a7ab1e8e42c81d8424cf41ef
sanieeme <shoneesani@gmail.com>
2024-07-29 14:40:13 +0000

    parameterize

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test cases for GithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """method that test"""
+        expected_response = {"mock": "data"}
+        mock_get_json.return_value = expected_response
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        mock_get_json.assert_called_once_with(client.ORG_URL.format(org=org_name))
+        self.assertEqual(result, expected_response)
+
+
+if __name__ == '__main__':
+    unittest.main()
