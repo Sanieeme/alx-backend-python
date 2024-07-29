f80db00b22658ec16fc36c8e352d8ea9a436f812
sanieeme <shoneesani@gmail.com>
2024-07-29 07:56:14 +0000

    parameterize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
+"""import modusle """
+import unittest
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+from utils import access_nested_map
 
+
+class TestAccessNestedMap(unittest.TestCase):
+    """class that inherits"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
+        """method """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
