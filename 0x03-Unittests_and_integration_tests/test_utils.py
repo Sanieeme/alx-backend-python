c2109ee1cf7d525b767a364793bfde3050806429
sanieeme <shoneesani@gmail.com>
2024-07-29 08:27:39 +0000

    parameterise a unit test update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
+        ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
         ])
     def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence, exception: type) -> None:
